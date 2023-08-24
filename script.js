17453d5773e3d0df3f66ff34dfc621ce8340a8c1
Martin <martinchoyx@gmail.com>
2023-08-24 10:06:07 -0300

    Arreglo de overflowY en chatbox2

--- script.js
+++ script.js
@@ -1,53 +1,41 @@
 const firstItems = document.querySelectorAll('.first');
-
-
 const header2 = document.querySelector('.header2');
 const chatbox2 = document.querySelector('.chatbox2');
-
+const clickableImages = document.querySelectorAll('.clickable-image');
+const overlay = document.getElementById('overlay');
+let chatbox2Overflow = 'auto';
 
 header2.style.display = 'none';
 chatbox2.style.display = 'none';
 
-
 firstItems.forEach(item => {
-  item.addEventListener('click', () => {
-    
-    item.classList.toggle('selected');
-    
-    
-    const header1 = document.querySelector('.header1');
-    const chatbox1 = document.querySelector('.chatbox1');
-    
-    header1.style.display = 'none';
-    chatbox1.style.display = 'none';
-    
-    header2.style.display = '';
-    chatbox2.style.display = '';
-  });
+    item.addEventListener('click', () => {
+        item.classList.toggle('selected');
+        
+        const header1 = document.querySelector('.header1');
+        const chatbox1 = document.querySelector('.chatbox1');
+        
+        header1.style.display = 'none';
+        chatbox1.style.display = 'none';
+        
+        header2.style.display = '';
+        chatbox2.style.display = 'block';
+    });
 });
 
-// Obtén todas las imágenes con la clase "clickable-image"
-const clickableImages = document.querySelectorAll('.clickable-image');
-
-// Agrega un event listener a cada imagen
 clickableImages.forEach(image => {
-  image.addEventListener('click', () => {
-    // Agrega o quita la clase "enlarged" para cambiar el tamaño de la imagen
-    image.classList.toggle('enlarged');
-  });
+    image.addEventListener('click', () => {
+        image.classList.toggle('enlarged');
+        
+        
+        if (overlay.style.display === 'block') {
+            overlay.style.display = 'none';
+            chatbox2Overflow = 'auto';
+        } else {
+            overlay.style.display = 'block';
+            chatbox2Overflow = 'hidden';
+        }
+        
+        chatbox2.style.overflowY = chatbox2Overflow;
+    });
 });
-
-const clickableImage = document.querySelector('.clickable-image');
-const overlay = document.getElementById('overlay');
-
-clickableImage.addEventListener('click', () => {
-    if (overlay.style.display === 'block') {
-        overlay.style.display = 'none';
-    } else {
-        overlay.style.display = 'block';
-    }
-});
-
-
-
- 
